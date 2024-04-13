1bbf901fa021446df5507027ff54b672bcf878d4
provot33 <s_provot@yahoo.fr>
2024-04-13 18:34:41 +0200

    (feat.) configuration pour permettre le déploiement

--- index.php
+++ index.php
@@ -13,9 +13,9 @@ define('_ROOTPATH_', __DIR__);
 define('_TEMPLATEPATH_', __DIR__.'/Template');
 spl_autoload_extensions(".php");
 spl_autoload_register();
-// spl_autoload_register(function ($class){
-//    require_once _ROOTPATH_. '/' . strtolower(str_replace('\\', '/', $class) . '.php');
-// });
+spl_autoload_register(function ($class){
+   require_once _ROOTPATH_. '/' . strtolower(str_replace('\\', '/', $class) . '.php');
+});
 
 use App\Controller\Controller;
 // Nous avons besoin de cette classe pour verifier si l'utilisateur est connecté
